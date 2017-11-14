83c579ac6bd1aeb513a84c7abc221099b04be9ba
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2017-11-14 16:07:37 +0800

    added data

--- KB_Analysis/conceptnet_api.py
+++ KB_Analysis/conceptnet_api.py
@@ -1,9 +1,12 @@
 import requests
 from pprint import pprint
 
-entity = 'hand'
-obj = requests.get('http://api.conceptnet.io/c/en/' + entity).json()
-# pprint(obj)
-
-for x in obj['edges']:
-    print(entity, '-', x['rel']['label'], '-', x['end']['label'])
+entities = ['shovel', 'dig']
+for entity in entities:
+    obj = requests.get('http://api.conceptnet.io/c/en/' + entity + '?offset=0&limit=1000').json()
+    pprint(len(obj['edges']))
+    for x in obj['edges']:
+        if x['end']['label'] == entity:
+            continue
+        print(entity, '-', x['rel']['label'], '-', x['end']['label'])
+    print("\n")
